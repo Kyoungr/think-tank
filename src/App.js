b679e0c8f8bee968f627373b84be7d58922c9ec7
Kevon Young-Reaves <kjyoungr@coastal.edu>
2019-10-06 17:51:00 -0400

    Create frontend, signin, register, homepage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Navigation from './components/Navigation/Navigation'
+import Logo from './components/Logo/Logo';
+import Signin from './components/Signin/Signin'
+import Register from "./components/Register/Register"
 import './App.css';
 
-function App() {
-  return (
+class App extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      input: '',
+      route: 'signin',
+      isSignedIn: false
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onRouteChange = (route) => {
+    if (route === "signout"){
+      this.setState({isSignedIn: false})
+    }else if (route === 'home'){
+      this.setState({isSignedIn: true})
+    }
+    this.setState({route: route});
+  }
+
+  render() {
+
+    const {isSignedIn, route } = this.state;
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+     {route === 'home' 
+     ? 
+     <div>
+       <Logo />
+     </div>
+     : (
+       this.state.route === 'signin'
+       ? <Signin onRouteChange={this.onRouteChange} /> 
+       : <Register onRouteChange={this.onRouteChange} />
+     )
+     }
     </div>
   );
+  }
 }
 
 export default App;
