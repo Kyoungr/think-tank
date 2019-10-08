819fc4432ca1b0e96b1427e35f8cdde4c329595f
Kevon Young-Reaves <kjyoungr@coastal.edu>
2019-10-07 20:42:55 -0400

    Server setup, register and signin modification

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Navigation from './components/Navigation/Navigation'
 import Logo from './components/Logo/Logo';
-import Signin from './components/Signin/Signin'
-import Register from "./components/Register/Register"
+import Signin from './components/Signin/Signin';
+import Register from "./components/Register/Register";
 import './App.css';
 
 class App extends Component {
+
 
   constructor(){
     super();
@@ -13,18 +14,37 @@ class App extends Component {
     this.state = {
       input: '',
       route: 'signin',
-      isSignedIn: false
+      isSignedIn: false,
+      user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
     }
   }
+
+  loadUser = (data) => {
+    this.setState({user: {
+      id: data.id,
+      name: data.name,
+      email: data.email,
+      entries: data.entries,
+      joined: data.joined
+    }})
+  }
+
+
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   }
 
   onRouteChange = (route) => {
-    if (route === "signout"){
+    if (route === 'signout') {
       this.setState({isSignedIn: false})
-    }else if (route === 'home'){
+    } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
     this.setState({route: route});
@@ -43,9 +63,9 @@ class App extends Component {
        <Logo />
      </div>
      : (
-       this.state.route === 'signin'
-       ? <Signin onRouteChange={this.onRouteChange} /> 
-       : <Register onRouteChange={this.onRouteChange} />
+      route === 'signin'
+      ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
+      : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
      )
      }
     </div>
